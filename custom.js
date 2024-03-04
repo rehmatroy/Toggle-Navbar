0d356836e45bbdd16b0e87748f0d5e16024fa62d
Rehmat Ali <74316215+rehmatroy@users.noreply.github.com>
2024-03-05 01:45:15 +0500

    toggle-navbar-commit

---
+++ custom.js
@@ -0,0 +1,6 @@
+const toggleBtn2 = document.getElementById('toggleBtn2');
+const navList = document.querySelector('.navList');
+
+toggleBtn2.addEventListener('click', () =>{
+  navList.classList.toggle('show');
+} );
